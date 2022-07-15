5a20c77982ae39d8927a714e9759436492fe0a70
yuvalavn <45373111+yuvalavn@users.noreply.github.com>
2022-07-15 05:21:12 +0300

    meson

--- setup.sh
+++ setup.sh
@@ -2,6 +2,7 @@ sudo apt install -y build-essential
 sudo apt-get install -y libncurses-dev gawk flex bison openssl libssl-dev dkms libelf-dev libudev-dev libpci-dev libiberty-dev autoconf
 sudo apt-get install -y build-essential cmake gcc libudev-dev libnl-3-dev libnl-route-3-dev ninja-build pkg-config valgrind python3-dev cython3 python3-docutils pandoc
 sudo apt install -y python3-pyelftools
+sudo apt install meson
 
 git clone -b longli/rebase_to_5.18-v4 https://github.com/yuvalavn/linux
 git clone -b rebase-upstream-v1 https://github.com/yuvalavn/rdma-core
