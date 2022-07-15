ac9ebdc89125b79b8e1e7aed66b140cf8f4b54ef
yuvalavn <45373111+yuvalavn@users.noreply.github.com>
2022-07-15 05:12:26 +0300

    y

--- setup.sh
+++ setup.sh
@@ -1,7 +1,7 @@
-sudo apt install build-essential
-sudo apt-get install libncurses-dev gawk flex bison openssl libssl-dev dkms libelf-dev libudev-dev libpci-dev libiberty-dev autoconf
-sudo apt-get install build-essential cmake gcc libudev-dev libnl-3-dev libnl-route-3-dev ninja-build pkg-config valgrind python3-dev cython3 python3-docutils pandoc
-apt install python3-pyelftools
+sudo apt install -y build-essential
+sudo apt-get install -y libncurses-dev gawk flex bison openssl libssl-dev dkms libelf-dev libudev-dev libpci-dev libiberty-dev autoconf
+sudo apt-get install -y build-essential cmake gcc libudev-dev libnl-3-dev libnl-route-3-dev ninja-build pkg-config valgrind python3-dev cython3 python3-docutils pandoc
+sudo apt install -y python3-pyelftools
 
 git clone -b longli/rebase_to_5.18-v4 https://github.com/yuvalavn/linux
 git clone -b rebase-upstream-v1 https://github.com/yuvalavn/rdma-core
